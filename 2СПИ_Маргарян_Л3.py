31312c4854d641c9d6c8bf6085a4623e6879daf3
pel1x <margaryan.damir12@gmail.com>
2024-10-10 12:37:25 +0900

    Add files via upload

--- 2СПИ_Маргарян_Л3.py
+++ 2СПИ_Маргарян_Л3.py
@@ -2,9 +2,23 @@ def main():
     
     # 1 задание 
 
-    print('a')
+    # while True:
+    #     x = input("Введите строку:")
+    #     if x == "":
+    #         break
+    #     print("Длина этой строки:", len(x))
 
+    # 2 задание
 
+    count = 0
+
+    while True:
+        x = float(input("Введите число:"))
+        if x > 36.6:
+            break
+        elif x <= 36.6:
+            count += 1
+    print("Количество отрицательных чиел:", count)
 
 if __name__ == "__main__":
     main()
