4df496bf683fdde850bb2f78adf0374d216c9ae8
pel1x <margaryan.damir12@gmail.com>
2025-01-17 14:13:47 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from horse2 import horse2
+from template import template
+from func_table import func_table
+
+
+def main():
+    horse2("e4")
+    template(2, 1, 4)
+    func_table()
+
+
+if __name__ == "__main__":
+    main()
