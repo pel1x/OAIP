2abda6fe3a0d3cf02640b3524ee8254d102e439b
pel1x <margaryan.damir12@gmail.com>
2025-01-19 17:31:24 +0800

    Add files via upload

--- horse2.py
+++ horse2.py
@@ -1,5 +1,26 @@
-def horse2(position, a=None):
+
+
+def horse2(position):
     moves = [
         (2, 1), (2, -1), (-2, 1), (-2, -1),
         (1, 2), (1, -2), (-1, 2), (-1, -2)
     ]
+    
+
+    stolb = position[0]
+    ryd = int(position[1])
+
+    stolb_index = 'abcdefgh'.index(stolb) + 1
+
+    possible_moves = []
+    
+    for move in moves:
+        new_stolb = stolb_index + move[0]
+        new_ryd = ryd + move[1]
+        
+        if 1 <= new_stolb <= 8 and 1 <= new_ryd <= 8:
+            new_position = 'abcdefgh'[new_stolb - 1] + str(new_ryd)
+            possible_moves.append(new_position)
+    
+    return possible_moves
+
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from horse2 import horse2
 from template import template
-from func_table import func_table
+
 
 
 def main():
-    horse2("e4")
-    template(2, 1, 4)
-    func_table()
+    horse2("a3")
+    template(4, 4, 4)
 
 
 if __name__ == "__main__":
